df9e62812b93909e2dccbdc24c24c13c0988d4f1
Roomsaver <tropicalpikachu2@gmail.com>
2017-09-13 13:20:10 -0400

    Add files via upload

---
+++ russian roulette.cpp
@@ -0,0 +1,42 @@
+//Russian Roulette
+//written by Wesley Lesher
+#include <iostream>
+#include <ctime>
+#include <chrono>
+#include <unistd.h>
+
+using namespace std;
+
+int main(){
+ srand(time(NULL));
+
+ bool dead = false;
+ int round = 1;
+ 
+ cout << "Russian Roulette" << endl;
+ 
+ 
+ while(round <= 6 && dead != true){
+        int random = rand()%6 + 1;
+        cout << "Round " << round;
+        usleep(300000);
+        cout << ".";
+        usleep(300000);
+        cout << ".";
+        usleep(300000);
+        cout << ".";
+     if(random == 5){
+       dead = true;
+       cout << "Bang!"<< endl << "You lost. You lasted " << round << " rounds." << endl;
+     }
+     else{
+       cout << "Click." << endl;
+       dead = false;
+     }
+     round++;
+ }
+ 
+    if(dead==false){
+        cout << "You won! You lasted all rounds." << endl;
+    }
+}
